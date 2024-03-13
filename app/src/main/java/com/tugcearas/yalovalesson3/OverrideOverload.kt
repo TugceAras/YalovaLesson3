7b53d5a0c91ac2ff6bd71cf2f062832a10a44e46
TugceAras <aras.tugcee@gmail.com>
2024-03-13 10:51:23 +0300

    Overload and override keywords explained

---
+++ app/src/main/java/com/tugcearas/yalovalesson3/OverrideOverload.kt
@@ -0,0 +1,22 @@
+package com.tugcearas.yalovalesson3
+
+// Function Overloading (Asiri yukleme) --> Iki veya daha fazla fonksiyonun ayn isme ancak farkli parametlere sahip olmasi anlamina gelir.
+
+fun myFun (number1:Int){}
+fun myFun (number1: Int, number2: Int){}
+fun myFun (number1:Int, number2:Int, myString:String){}
+
+//Override Ise (Gecersiz kilma) --> Ust siniftaki metodun alt sinifta gorevinin degistirilerek kullanilmasi durumudur. (draw icindeki print`lere dikkat)
+//Artik Circle sinifi icindeki draw metodu cagrildiginda "Sekil ciziliyor" degil de "Daire ciziliyor" ciktisi alinir. Cunku gorevini degistirdik.
+
+open class Shape {
+    open fun draw() {
+        println("Sekil çiziliyor.")
+    }
+}
+
+class Circle : Shape() {
+    override fun draw() {
+        println("Daire çiziliyor.")
+    }
+}
