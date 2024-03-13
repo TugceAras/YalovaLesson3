a0bcf58b752b188022db08eb63ee365cecb7007a
TugceAras <aras.tugcee@gmail.com>
2024-03-13 10:58:29 +0300

    comment lines added

--- app/src/main/java/com/tugcearas/yalovalesson3/OverrideOverload.kt
+++ app/src/main/java/com/tugcearas/yalovalesson3/OverrideOverload.kt
@@ -7,6 +7,7 @@ fun myFun (number1: Int, number2: Int){}
 fun myFun (number1:Int, number2:Int, myString:String){}
 
 //Override Ise (Gecersiz kilma) --> Ust siniftaki metodun alt sinifta gorevinin degistirilerek kullanilmasi durumudur. (draw icindeki print`lere dikkat)
+// Ust sinif burada --> Shape, alt sinifta --> Circle
 //Artik Circle sinifi icindeki draw metodu cagrildiginda "Sekil ciziliyor" degil de "Daire ciziliyor" ciktisi alinir. Cunku gorevini degistirdik.
 
 open class Shape {
